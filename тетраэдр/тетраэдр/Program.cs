9f2ad3afe6bdcfeae11f8ad6890eb8dc88b37876
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2020-10-12 21:13:42 +0500

    Add files via upload

---
+++ тетраэдр/тетраэдр/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+namespace тетраэдр
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x;
+            double b;
+            double c;
+           Console.WriteLine("Введите длину ребра тетраэдра:");
+            x = double.Parse(Console.ReadLine());
+           
+            b = x*x*x*Math.Sqrt(2)/12;
+            c = x * x * Math.Sqrt(3);
+
+            Console.WriteLine("Объём тетраэдра:" + b);
+            Console.WriteLine("Площадь поверхности тетраэдра:" + c);
+
+
+            Console.ReadKey();
+        }
+    }
+}
