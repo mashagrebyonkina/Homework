df2b06af3811e9a0ff0f576dd4ae57b0c5498777
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2020-10-12 21:06:32 +0500

    Add files via upload

---
+++ Арифметика/Арифметика/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Арифметика
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, y;
+            Console.WriteLine("Введите трёхзначное число");
+            //x = (Console.ReadLine());
+            x = int.Parse(Console.ReadLine());
+            y = ((x/10) + (x%10)*100);
+            Console.WriteLine(y);
+            Console.ReadKey();
+            
+
+        }
+    }
+}
