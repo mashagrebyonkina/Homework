8f5f0009ea7f5634a272a7e9b4afb287501e0ad5
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2020-11-17 20:08:40 +0500

    Add files via upload

---
+++ task6.1/task6.1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace task6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числo m");
+            var m = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите числo n");
+            var n = int.Parse(Console.ReadLine());
+
+            string answer = "Нет.";
+            if (EvaluateLogExpression(m, n))
+                answer = "Да.";
+
+            Console.WriteLine($"Верно ли, что каждое из чисел m и n нечетное? -" + answer);
+
+        }
+        
+        static bool EvaluateLogExpression (double m, double n)
+        {
+            //Каждое из чисел m и n нечетное
+
+            return m % 2 !=0 && n % 2 != 0;
+        }
+    } 
+     
+}
