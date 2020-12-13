29102327c43a2986241543340ffe65fd7e6071d9
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2020-12-13 22:52:36 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,28 +1,29 @@
 ﻿using System;
 
-namespace task_8._5
+namespace task_8._4
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a, b, c, d, e");
-            var a = int.Parse(Console.ReadLine());
-            var b = int.Parse(Console.ReadLine());
-            var c = int.Parse(Console.ReadLine());
-            var d = int.Parse(Console.ReadLine());
-            var e = int.Parse(Console.ReadLine());
-            var x = 0.0;
-            while (Math.Pow(x, 4) - a * Math.Pow(x, 3) - b * Math.Pow(x, 2) - c * x - d < 0)
-            {
-                x++;
+                Console.WriteLine("Введите число n");
+                var n = int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Введите k");
+                var k = int.Parse(Console.ReadLine());
+                var s = 0;
+                while (n != 0)
+                {
+                    if (k < n % 10)
+                    {
+                        s += n % 10;
+                    }
+                    n = n / 10;
+                }
+                Console.WriteLine(s);
             }
-            while ((x) / 2.0 > 2 * e)
-            {
-                x = x / 2;
-            }
-            Console.WriteLine(x);
-            Console.ReadKey();
         }
     }
-}
+
+    
+
