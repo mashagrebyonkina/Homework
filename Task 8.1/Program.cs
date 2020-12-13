41faecfa221d89061ccba80f68adc566f2252871
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2020-12-13 22:58:45 +0500

    Add files via upload

---
+++ Task 8.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Task_8._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var sum = 0.0;
+            for (var i = 1; i <= n; i++)
+            {
+                sum += i / (i + 1.0);
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
