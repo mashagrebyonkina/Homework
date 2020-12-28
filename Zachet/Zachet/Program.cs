a6cb5ab4d635f7bab1da6f07e43e18d969ac2866
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2020-12-28 12:45:07 +0500

    Add files via upload

---
+++ Zachet/Zachet/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace Zachet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите натуральное число");
+            int n = new int();
+            while (true)
+            { 
+                var UserNumb = (Console.ReadLine());
+                if (!int.TryParse(UserNumb, out n) || !(int.Parse(UserNumb) > 0)) 
+                    {
+                    Console.WriteLine("Ошибка ввода.Попробуйте ещё раз.");
+                    continue;
+                }
+                else
+                {
+                    Answer(n);
+                    break;
+                }
+            }
+        }
+        static void Answer (int n)
+        {
+            int amount = 0;
+            int maxnumb = 0;
+
+            while(n!=1)
+            {
+                amount +=1;
+                if (n>maxnumb)
+                {
+                    maxnumb = n;
+                }
+                if (n%2==0)
+                {
+                    n /= 2;
+                }
+                else 
+                {
+                    n *= 3;
+                    n += 1;
+                }
+            }
+            Console.WriteLine($"Цепочка состоит из {amount} элементов. Максимальное значение в цепи - {maxnumb}");
+        }
+    }
+}
