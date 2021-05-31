13e46cb8950fa78b8394ef39461c21c5de44de65
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2021-05-31 19:54:19 +0500

    Add files via upload

---
+++ HotelLibrary/HotelLibrary/JuniorSuite.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelLibrary
+{
+    public class JuniorSuite : Room
+    {
+        public bool Wash, BookingTable, CleanUp, OrderTaxi;
+
+        public JuniorSuite(int roomNumber, int bedsCount, DirectionOfWindows windowDirection, List<bool> calls) 
+            : base(roomNumber, bedsCount, windowDirection)
+        {
+            Wash = calls[0];
+            BookingTable = calls[1];
+            CleanUp = calls[2];
+            OrderTaxi = calls[3];
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine(this);
+            Console.WriteLine(
+                "Статус доплнительных услуг:" + "\n" +
+                "------------------------------------------------------------------------" + "\n" +
+                $"Стирка вещей: {GetAnswer(Wash)}" + "\n" +
+                $"Бронирование стола в ресторане: {GetAnswer(BookingTable)}" + "\n" +
+                $"Чистка комнаты: {GetAnswer(CleanUp)}" + "\n" +
+                $"Заказ такси: {GetAnswer(OrderTaxi)}");
+            
+            string GetAnswer(bool service) => service ? "назначено" : "не назначено";
+        }
+    }
+}
---
+++ HotelLibrary/HotelLibrary/Lux.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HotelLibrary
+{
+    public class Lux : JuniorSuite
+    {
+        public int CountRooms;
+        public int MinRentDuration;
+        
+        public Lux(
+            int roomNumber, int bedsCount, DirectionOfWindows windowDirection, List<bool> calls, int countRooms, int minRentDuration) 
+            : base(roomNumber, bedsCount, windowDirection, calls)
+        {
+            CountRooms = countRooms;
+            MinRentDuration = minRentDuration;
+        }
+
+        public override void PrintInfo()
+        {
+            Console.WriteLine(this);
+            Console.WriteLine(
+                $"Количество комнат: {CountRooms}" + "\n" +
+                $"Минимальная продолжительность аренды: {MinRentDuration}");
+        }
+    }
+}
