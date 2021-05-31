f9467d860c8d9d9002dc45d4fd397c0e2ce6b1d3
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2021-05-31 19:51:22 +0500

    Add files via upload

---
+++ HotelLibrary/HotelLibrary/Room.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace HotelLibrary
+{
+    public class Room
+    {
+        public readonly int RoomNumber; 
+        public int BedsCount;
+        public DirectionOfWindows WindowDirection;
+        public int PricePerDay;
+        public DateTime WhenVacant;
+
+        public Room(int roomNumber, int bedsCount, DirectionOfWindows windowDirection)
+        {
+            RoomNumber = roomNumber;
+            BedsCount = bedsCount;
+            WindowDirection = windowDirection;
+        }
+        
+        public override string ToString()
+        {
+            return $"Номер: {RoomNumber}. Кол-во кроватей: {BedsCount}.";
+        }
+        
+        public virtual void PrintInfo()
+        {
+            Console.WriteLine(this);
+
+            var windowDirection = "";
+            switch (WindowDirection)
+            {
+                case DirectionOfWindows.East:
+                    windowDirection = "восток";
+                    break;
+                case DirectionOfWindows.West:
+                    windowDirection = "запад";
+                    break;
+                case DirectionOfWindows.North:
+                    windowDirection = "север";
+                    break;
+                case DirectionOfWindows.South:
+                    windowDirection = "юг";
+                    break;
+            }
+
+            Console.WriteLine(
+                $"Окна: направлены на {windowDirection}. " +
+                $"Цена: {PricePerDay} руб./сутки. " +
+                $"Номер будет свободен: {WhenVacant.ToShortDateString()} {WhenVacant.ToShortTimeString()}.");
+        }
+
+    }
+}
