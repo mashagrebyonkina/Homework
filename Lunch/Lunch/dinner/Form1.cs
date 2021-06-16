e4168f47b2f8344cd7242d5b0e53232a50950268
mashagrebyonkina <72135701+mashagrebyonkina@users.noreply.github.com>
2021-06-16 19:27:08 +0500

    Задание 13 выполнено

---
+++ Lunch/Lunch/dinner/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            #region Чтение данных
+
+            var pathForRead = @"C:\Users\Мария\Desktop\Lunch\Lunch\Menu.txt";
+            var input = "";
+            
+            try
+            {
+                using var sr = new StreamReader(pathForRead);
+                input = sr.ReadToEnd();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+            #endregion
+
+            #region Форматирование данных и добавление меню
+
+            var dishes = input
+                .Split(';')
+                .Select(str => str.Split())
+                .Select(pair => new Dish(pair[0], Convert.ToInt32(pair[1])))
+                .ToList();
+
+            checkedListBox1.DataSource = dishes;
+
+            #endregion
+        }
+
+        private void calculation_Click(object sender, EventArgs e)
+        {
+            var sum = checkedListBox1.CheckedItems
+                .Cast<Dish>()
+                .Select(x => x.Price)
+                .Sum();
+            
+            label1.Text = $"Сумма заказа: {sum}";
+        }
+    }
+
+    class Dish
+    {
+        public readonly string Name;
+        public readonly int Price;
+
+        public Dish(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString() => $"{Name}: {Price} руб.";
+    }
+}
---
+++ Lunch/Lunch/dinner/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("WindowsFormsApplication1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WindowsFormsApplication1")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("3599148d-a7f4-41de-b7ec-8269d08b4ff4")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
